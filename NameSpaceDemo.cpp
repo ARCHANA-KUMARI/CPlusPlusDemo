df17151ae407a3ca1d902dcf71b43832c1adc421
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-19 00:54:51 +0530

    Accessing display1 function from room4 namespace

--- NameSpaceDemo.cpp
+++ NameSpaceDemo.cpp
@@ -18,11 +18,26 @@ namespace room3{
     void display(){
         std::cout << "This is room 3" << std::endl;
     }
+
 }
+
+namespace room4{
+    void display() {
+        std::cout << "This is room 4" << std::endl;
+    }
+
+    void display1() {
+        std::cout << "This is room 4 for display1: " << std::endl;
+    }
+}
+
 using namespace room3; // Using room3 namespace
+using room4::display1; // Using room4::display1 function`
 int main() {
     room1::display(); // Accessing display function from room1 namespace
     room2::display(); // Accessing display function from room2 namespace
     display(); // Accessing display function from room3 namespace (using directive)
+    room4::display(); // Accessing display function from room4 namespace using :: operator
+    display1(); // Accessing display1 function from room4 namespace
     return 0;
 }
