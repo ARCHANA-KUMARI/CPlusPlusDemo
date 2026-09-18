9dd57a0c0a70361c7f5658d9a2206da0bdb0fd83
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-19 00:43:03 +0530

    ccessing display function from room3 namespace (using directive)

--- NameSpaceDemo.cpp
+++ NameSpaceDemo.cpp
@@ -13,8 +13,16 @@ namespace room2{
         std::cout << "This is room 2" << std::endl;
     }
 }
+
+namespace room3{
+    void display(){
+        std::cout << "This is room 3" << std::endl;
+    }
+}
+using namespace room3; // Using room3 namespace
 int main() {
-    room1::display();
-    room2::display();
+    room1::display(); // Accessing display function from room1 namespace
+    room2::display(); // Accessing display function from room2 namespace
+    display(); // Accessing display function from room3 namespace (using directive)
     return 0;
 }
