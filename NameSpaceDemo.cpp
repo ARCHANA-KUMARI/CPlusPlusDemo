7821273f6beccb7b1a3431252741dc683c97a131
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-19 00:35:27 +0530

    NameSpace demo

--- NameSpaceDemo.cpp
+++ NameSpaceDemo.cpp
@@ -1,3 +1,20 @@
 //
 // Created by archana-kumari on 9/18/26.
 //
+#include <iostream>
+namespace room1{
+    void display(){
+        std::cout << "This is room 1" << std::endl;
+    }
+}
+
+namespace room2{
+    void display(){
+        std::cout << "This is room 2" << std::endl;
+    }
+}
+int main() {
+    room1::display();
+    room2::display();
+    return 0;
+}
