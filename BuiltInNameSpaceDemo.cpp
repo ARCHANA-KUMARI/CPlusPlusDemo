2a1f90a014787ed96bd71e1c5f6c0540f361b493
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-19 10:28:20 +0530

    Builtin Namespace demo

---
+++ BuiltInNameSpaceDemo.cpp
@@ -0,0 +1,12 @@
+//
+// Created by archana-kumari on 9/19/26.
+//
+using namespace std;
+#include <iostream>
+int main() {
+    int a = 3, b = 7;
+
+    // 'cout' and 'endl' are part of the std namespace
+    cout << "Sum: " << a + b << endl;
+    return 0;
+}
