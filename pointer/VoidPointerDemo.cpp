694d7a59de0f2575426ba880a8f226af47dc65e5
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-21 02:27:25 +0530

    Void pointer demo

---
+++ pointer/VoidPointerDemo.cpp
@@ -0,0 +1,21 @@
+//
+// Created by archana-kumari on 9/21/26.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 42;
+
+    // void pointer holding address of an int
+    void *ptr = &x;
+
+    // Error: cannot dereference void pointer
+    // cout << *ptr;
+
+    // Typecast before dereferencing
+    cout << "Value pointed by void pointer: " << *(static_cast<int *>(ptr)) << endl;
+
+    return 0;
+}
