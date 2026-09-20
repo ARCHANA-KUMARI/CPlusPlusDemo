945e59988630be5038b9cc0006a32dfcde7e7311
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-21 02:12:01 +0530

    Modify address of pointed pointer

---
+++ pointer/ModifyAddressPointerDemo.cpp
@@ -0,0 +1,20 @@
+//
+// Created by archana-kumari on 9/20/26.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 99;
+
+    int *ptr = &a;
+    cout << *ptr << endl;
+
+    // Changing the address stored
+    ptr = &b;
+    cout << *ptr;
+
+    return 0;
+}
