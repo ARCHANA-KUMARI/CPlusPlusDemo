c3c5534e78f19838c4786d9e2fb3e9fd215ed620
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-21 02:15:47 +0530

    Null pointer

---
+++ pointer/NullPointerDemo.cpp
@@ -0,0 +1,14 @@
+//
+// Created by archana-kumari on 9/21/26.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // nullptr pointer
+    int *ptr = nullptr;
+    cout << "Value of ptr: " << ptr << endl; // Output: Value of ptr: 0
+    return 0;
+}
