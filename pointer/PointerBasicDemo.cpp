15d5e732073074a891c62fb97df887f744dc5792
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-20 23:22:14 +0530

    Pointer basic concept demo

---
+++ pointer/PointerBasicDemo.cpp
@@ -0,0 +1,22 @@
+//
+// Created by archana-kumari on 9/20/26.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int var = 10;
+
+    // declare pointer and store address of var
+    int *ptr = &var;
+
+    // print value and address
+    cout << "Value of var: " << var << endl;
+    cout << "Address of var: " << &var << endl;
+
+    cout << "Value stored in pointer ptr: " << ptr << endl;
+    cout << "Value pointed to by ptr: " << *ptr << endl;
+
+    return 0;
+}
