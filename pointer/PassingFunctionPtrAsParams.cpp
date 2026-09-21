a29162d3b1b2b188bc9dbe99d39bdd9f4e781e3c
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-21 16:03:22 +0530

    passing function pointer as params of function

---
+++ pointer/PassingFunctionPtrAsParams.cpp
@@ -0,0 +1,36 @@
+//
+// Created by archana-kumari on 9/21/26.
+//
+using namespace std;
+
+#include <iostream>
+
+const int a = 15;
+const int b = 2;
+
+int multiply() {
+    return a * b;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+void print(int (*funcPtr)(int, int), int x, int y) {
+    cout << "Result: " << funcPtr(x, y) << endl;
+}
+
+void print(int (*funcPtr)()) {
+    cout << "Result: " << funcPtr() << endl;
+}
+
+int main() {
+
+    print(multiply);
+    print(add, 10, 5);
+    return 0;
+
+    return 0;
+}
+
+
