83408d3d12a6ce774ae353a013720f1012de04ff
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-21 23:42:45 +0530

    Reference demo

---
+++ pointer/References.cpp
@@ -0,0 +1,22 @@
+//
+// Created by archana-kumari on 9/21/26.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 10;
+
+    // ref is a reference to x.
+    int &ref = x;
+
+    // printing value using ref
+    cout << ref << endl;
+
+    // Changing the value and printing again
+    ref = 22;
+    cout << ref<<endl;
+
+    return 0;
+}
