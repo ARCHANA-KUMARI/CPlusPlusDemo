6ccb0f2d4dc6a555cec71091c9189ae41955ad52
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-21 15:36:06 +0530

    Function pointer demo

---
+++ pointer/FunctionPointerDemo.cpp
@@ -0,0 +1,22 @@
+//
+// Created by archana-kumari on 9/21/26.
+//
+using namespace std;
+
+#include <iostream>
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    // Declare a function pointer and assign it to the multiply function
+    int (*funcPtr1)(int, int);
+    funcPtr1 = multiply;
+
+    // Call the function using the function pointer
+    int result = funcPtr1(5, 3);
+    cout << "Result: " << result << endl; // Output: Result: 15
+
+    return 0;
+}
