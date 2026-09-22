c3489fd181c31ebe4a867133a44b53a5981d299b
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-23 02:52:19 +0530

    opps concept : Copy Constructor

---
+++ oops/constructor/CopyConstructorDemo.cpp
@@ -0,0 +1,30 @@
+//
+// Created by archana-kumari on 9/23/26.
+//
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    int val;
+
+    // Parameterized constructor
+    A(int x) {
+        val = x;
+    }
+
+    // Copy constructor
+    A(A& a) {
+        val = a.val;
+    }
+};
+
+int main() {
+    A a1(20);
+
+    // Creating another object from a1
+    A a2(a1);
+
+    cout << a2.val<<endl; // Output: 20
+    return 0;
+}
