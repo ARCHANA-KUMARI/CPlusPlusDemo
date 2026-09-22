380e29eb723e8c81aa3e5728dcb01bc08a403d32
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-23 02:27:03 +0530

    opps concept : Default Constructor

---
+++ oops/constructor/DefaultConstructorDemo.cpp
@@ -0,0 +1,20 @@
+//
+// Created by archana-kumari on 9/23/26.
+//
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+
+    // Constructor without any parameters
+    A() {
+        cout << "Default Constructor called" << endl;
+    }
+};
+
+int main() {
+    A obj1;
+    return 0;
+}
