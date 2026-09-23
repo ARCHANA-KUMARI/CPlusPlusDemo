0dfb46ce19c39253604c59415143683e36b856cc
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-09-23 13:28:38 +0530

    opps concept : Move Constructor

---
+++ oops/constructor/MoveConstructorDemo.cpp
@@ -0,0 +1,29 @@
+//
+// Created by archana-kumari on 9/23/26.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MyClass {
+private:
+    int b;
+
+public:
+    // Constructor
+    MyClass(int &&a) : b(move(a)) {
+        cout << "Move constructor called!" << endl;
+    }
+
+    void display() {
+        cout << b << endl;
+    }
+};
+
+int main() {
+    int a = 4;
+    MyClass obj1(move(a));  // Move constructor is called
+    obj1.display();
+    return 0;
+}
